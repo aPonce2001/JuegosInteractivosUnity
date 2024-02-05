e4de5106598421379c450dd6fdd1bbb1c4d09157
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2024-02-05 01:13:28 -0500

    laboratory(3DPlatformer): lab 14

--- 3DPlatformer/Assets/Scripts/PlayerController.cs
+++ 3DPlatformer/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,20 @@ public class PlayerController : MonoBehaviour
     public float gravityScale = 5f;
     public CharacterController characterController;
     private Vector3 _moveDirection;
+    private Camera theCamera;
+    public GameObject playerModel;
+    public Animator animator;
 
     void Start()
     {
-        return;
+        theCamera = Camera.main;
     }
 
     void Update()
     {
         float yStore = _moveDirection.y;
-        _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        _moveDirection = Input.GetAxisRaw("Vertical") * transform.forward + Input.GetAxisRaw("Horizontal") * transform.right;
+        _moveDirection = _moveDirection.normalized;
         _moveDirection *= moveSpeed;
         _moveDirection.y = yStore;
 
@@ -28,5 +32,14 @@ public class PlayerController : MonoBehaviour
         _moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
 
         characterController.Move(_moveDirection * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, theCamera.transform.rotation.eulerAngles.y, 0f);
+            Quaternion modelRotation = Quaternion.LookRotation(new Vector3(_moveDirection.x, 0f, _moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, modelRotation, Time.deltaTime * 10f);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(_moveDirection.x) + Mathf.Abs(_moveDirection.z));
     }
 }
